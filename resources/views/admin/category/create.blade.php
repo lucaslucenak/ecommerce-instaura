318d153e57ffcb659a589b2704eb72d517b65048
Lucas Lucena <lucas.lucenak@gmail.com>
2023-02-11 13:17:31 -0300

    Update create view: Add validation

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -11,19 +11,28 @@
                 </div>
                 <div class="card-body">
                     <form action="{{url('admin/category')}}" method="POST" enctype="multipart/form-data">
-                        @csrf
+                        @csrf 
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label for="">Name</label>
                                 <input type="text" name="name" class="form-control">
+                                @error('name')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="">Slug</label>
                                 <input type="text" name="slug" class="form-control">
+                                @error('slug')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="">Description</label>
                                 <textarea type="text" name="description" class="form-control"></textarea>
+                                @error('description')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="">Image</label>
@@ -39,14 +48,23 @@
                             <div class="col-md-6 mb-3">
                                 <label for="">Meta Title</label>
                                 <input type="text" name="meta_title" class="form-control">
+                                @error('meta_title')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <label for="">Meta Keyword</label>
                                 <textarea type="text" name="meta_keyword" class="form-control" rows="3"></textarea>
+                                @error('meta_keyword')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <label for="">Meta Description</label>
                                 <textarea type="text" name="meta_description" class="form-control" rows="3"></textarea>
+                                @error('meta_description')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <button type="submit" class="btn btn-primary float-end">Save</button>
