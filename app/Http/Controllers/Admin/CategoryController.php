4760da9251905678f4039362f0e67a1540d1503d
Lucas Lucena <lucas.lucenak@gmail.com>
2023-02-16 22:25:51 -0300

    Add edit category view

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -48,6 +48,6 @@ class CategoryController extends Controller
     }
 
     public function edit(Category $category) {
-        return $category;
+        return view('admin.category.edit', compact('category'));
     }
 }
