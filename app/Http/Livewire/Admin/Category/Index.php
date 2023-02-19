69c2c0ad797ea82731ad1d17018fd7b1900bd8e1
Lucas Lucena <lucas.lucenak@gmail.com>
2023-02-19 02:06:58 -0300

    Revert "Revert"
    This reverts commit a679afce40ccffe59ecfd861dd86cfe5f1eca8b8.

--- app/Http/Livewire/Admin/Category/Index.php
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -5,7 +5,6 @@ namespace App\Http\Livewire\Admin\Category;
 use Livewire\Component;
 use App\Models\Category;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\File;
 
 
 class Index extends Component
@@ -16,18 +15,8 @@ class Index extends Component
     public $category_id;
 
     public function deleteCategory($category_id) {
+        dd($category_id);
         $this->category_id = $category_id;
-    }
-
-    public function destroyCategory() {
-        $category = Category::find($this->category_id);
-        $path = 'uploads/category/'. $category->image;
-
-        if (File::exists($path)) {
-            File::delete($path);
-        }
-        $category->delete();
-        session()->flash('message', 'Category deleted');
     }
 
     public function render()
