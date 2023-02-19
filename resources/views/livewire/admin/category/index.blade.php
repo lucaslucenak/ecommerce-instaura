ce1ae68271074119d217c095ab45483e297de0a4
Lucas Lucena <lucas.lucenak@gmail.com>
2023-02-19 02:07:54 -0300

    Revert "Revert "Revert""
    This reverts commit 69c2c0ad797ea82731ad1d17018fd7b1900bd8e1.

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div wire:ignore.self class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
