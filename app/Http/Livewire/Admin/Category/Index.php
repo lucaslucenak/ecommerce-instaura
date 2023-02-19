ece395e95b02855334dfa61d469c3e03733d0266
Lucas Lucena <lucas.lucenak@gmail.com>
2023-02-18 23:53:31 -0300

    Add delete category function

--- app/Http/Livewire/Admin/Category/Index.php
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -12,6 +12,13 @@ class Index extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
+    public $category_id;
+
+    public function destroyCategory($category_id) {
+        dd($category_id);
+        $this->category_id = $category_id;
+    }
+
     public function render()
     {
         $categories = Category::orderBy('id', 'DESC')->paginate(10);
--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,44 +1,73 @@
-<div class="row">
-    <div class="col-md-12">
-
-        @if (session('message'))
-            <div class="alert alert-success">{{ session('message') }}</div>
-        @endif
-        <div class="card">
-            <div class="card-header">
-                <h3>Category
-                    <a href="{{ url('admin/category/create') }}" class="btn btn-primary btn-sm float-end">Add Category</a>
-                </h3>
+<div>
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Category delete</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-            <div class="card-body">
-                <table class="table table-borded table-striped">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Status</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($categories as $category)
-                            <tr>
-                                <td>{{$category->id}}</td>
-                                <td>{{$category->name}}</td>
-                                <td>{{$category->status == '1' ? 'Hidden' : 'Visible'}}</td>
-                                <td>
-                                    <a href="{{url('admin/category/'.$category->id.'/edit')}}" class="btn btn-success">Edit</a>
-                                    <a href="" class="btn btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
 
-                <div>
-                    {{$categories->links()}}
+            <form wire:submit.prevent="destroyCategory">
+                <div class="modal-body">
+                    <h6>Are you sure to delete the category?</h6>
+                  </div>
+      
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Yes, delete</button>
+                  </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+    
+            @if (session('message'))
+                <div class="alert alert-success">{{ session('message') }}</div>
+            @endif
+            <div class="card">
+                <div class="card-header">
+                    <h3>Category
+                        <a href="{{ url('admin/category/create') }}" class="btn btn-primary btn-sm float-end">Add Category</a>
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-borded table-striped">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($categories as $category)
+                                <tr>
+                                    <td>{{$category->id}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td>{{$category->status == '1' ? 'Hidden' : 'Visible'}}</td>
+                                    <td>
+                                        <a href="{{url('admin/category/'.$category->id.'/edit')}}" class="btn btn-success">Edit</a>
+                                        <a href="" wire:click="destroyCategory({{$category->id}})" data-toggle="modal" data-target="#deleteModal" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+    
+                    <div>
+                        {{$categories->links()}}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
