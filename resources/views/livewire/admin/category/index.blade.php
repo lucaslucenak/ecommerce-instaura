3cc21da61c2d94a85122bf53636e776de5353e57
Lucas Lucena <lucas.lucenak@gmail.com>
2023-02-23 19:23:46 -0300

    Update admin>category>index.blade

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,39 +1,35 @@
 <div>
-    <div wire:ignore.self class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Category delete</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-
-            <form wire:submit.prevent="destroyCategory">
-                <div class="modal-body">
-                    <h6>Are you sure to delete the category?</h6>
-                  </div>
-      
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Yes, delete</button>
-                  </div>
-            </form>
-
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="row">
+    <div class="row">
         <div class="col-md-12">
-    
+
             @if (session('message'))
                 <div class="alert alert-success">{{ session('message') }}</div>
             @endif
             <div class="card">
                 <div class="card-header">
                     <h3>Category
-                        <a href="{{ url('admin/category/create') }}" class="btn btn-primary btn-sm float-end">Add Category</a>
+                        <a href="{{ url('admin/category/create') }}" class="btn btn-primary btn-sm float-end">Add
+                            Category</a>
                     </h3>
                 </div>
                 <div class="card-body">
@@ -49,25 +45,27 @@
                         <tbody>
                             @foreach ($categories as $category)
                                 <tr>
-                                    <td>{{$category->id}}</td>
-                                    <td>{{$category->name}}</td>
-                                    <td>{{$category->status == '1' ? 'Hidden' : 'Visible'}}</td>
+                                    <td>{{ $category->id }}</td>
+                                    <td>{{ $category->name }}</td>
+                                    <td>{{ $category->status == '1' ? 'Hidden' : 'Visible' }}</td>
                                     <td>
-                                        <a href="{{url('admin/category/'.$category->id.'/edit')}}" class="btn btn-success">Edit</a>
-                                        <a href="#" wire:click="deleteCategory({{$category->id}})" data-toggle="modal" data-target="#deleteModal" class="btn btn-danger">Delete</a>
+                                        <a href="{{ url('admin/category/' . $category->id . '/edit') }}" class="btn btn-success">Edit</a>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#deleteModal">
+                                            <a href="#" wire:click="deleteCategory({{ $category->id }})">Delete</a>
+                                            
+                                        </button>
+
                                     </td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
-    
+
                     <div>
-                        {{$categories->links()}}
+                        {{ $categories->links() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
